c44c09ec0efdcec79252e7516d0a299c5f5ac7fc
gv <afairless@gmail.com>
2025-03-23 16:42:07 -0400

    remove italics from gruvbox

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -91,6 +91,15 @@ require('bufferline').setup()
 require('ibl').setup()
 require('toggleterm').setup()
 
+require('gruvbox').setup({
+    italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false,
+    },
+})
 vim.cmd([[colorscheme gruvbox]])
 
 cmd([[highlight link CompeDocumentation NormalFloat]])
